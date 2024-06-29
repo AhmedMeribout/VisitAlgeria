af04d3b1b462b5596a335b7eb7ba6147ac9e5930
AhmedMeribout <102850406+AhmedMeribout@users.noreply.github.com>
2024-06-29 21:13:44 +0200

    added the pages folder an components folder

---
+++ src/components/Home/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className='flex justify-center'>
+            {/* Add your navigation links here */}
+            <div className='text-gray-600 tex'> NavBar here</div>
+        </nav>
+    );
+};
+
+export default NavBar;
